6875e00db00975aefefeaff9509b13bae29cbd9b
Alexi Rahman <alexi.rahman@r76.se>
2026-03-06 22:46:08 +0100

    Put price in cache after getting the previous price (#22)

--- src/get_price_change.rs
+++ src/get_price_change.rs
@@ -20,18 +20,18 @@ pub fn get_price_change(ticker: &str, price: f64, increment: i64) -> PriceChange
     let current_floor_price = parse_floor_price(price);
 
     let mut last_floor_prices = LAST_FLOOR_PRICES.lock().unwrap();
-    last_floor_prices.insert(ticker.to_string(), current_floor_price);
-
     if !last_floor_prices.contains_key(ticker) {
-        return PriceChange::NoChange;
+      last_floor_prices.insert(ticker.to_string(), current_floor_price);
+      return PriceChange::NoChange;
     }
 
-    let last_price = last_floor_prices.get(ticker).unwrap();
+    let last_price = *last_floor_prices.get(ticker).unwrap();
+    last_floor_prices.insert(ticker.to_string(), current_floor_price);
 
-    if last_floor_price_diff(*last_price, current_floor_price) > increment as f64 {
-        if current_floor_price < *last_price {
+    if last_floor_price_diff(last_price, current_floor_price) > increment as f64 {
+        if current_floor_price < last_price {
             return PriceChange::Down;
-        } else if current_floor_price > *last_price {
+        } else if current_floor_price > last_price {
             return PriceChange::Up;
         }
     }
